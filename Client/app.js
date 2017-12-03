d20a16831ff718faa25be75b7e885a8cc227b27c
Inga Pflaumer <inga.pflaumer@gmail.com>
2017-12-03 16:01:17 +1100

    Initial stage rendered

--- Client/app.js
+++ Client/app.js
@@ -5,5 +5,36 @@ ipcRenderer.on('GSO', (event, arg) => {
 })
 
 function renderGame(gso){
+	//Assign hero and villain for story mode
+	//mode is taken from gso.mode
+	let hero;
+	let villain;
 
+	if(gso.players[gso.activePlayer].alignment=="hero"){
+		hero = gso.players[gso.activePlayer];
+		villain = gso.players[Math.abs(gso.activePlayer-1)];
+	} else {
+		hero = gso.players[Math.abs(gso.activePlayer-1)];
+		villain = gso.players[gso.activePlayer];
+	}
+
+	document.getElementById("villainAvatar").innerHTML = villain.name;
+	document.getElementById("heroAvatar").innerHTML = hero.name;
+
+	displayCards(hero, "heroHand");
+	displayCards(villain, "villainHand");
+}
+
+
+function displayCards(player, selector){
+	console.log(player.name);
+	let emptyCards = document.querySelectorAll("#"+selector+" div.card");
+
+	for(let i=0; i<5; i++){
+		if(player.cards[i]){
+			emptyCards[i].innerHTML = player.cards[i].name;
+		} else {
+			emptyCards[i].innerHTML = "empty";
+		}
+	}
 }
